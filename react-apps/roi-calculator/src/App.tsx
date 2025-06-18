747abb08cd6665cfa9d4833892f74fdcf7fedf32
dimash <dimash@coveofedu.org>
2025-06-19 00:14:08 +0500

    change to vite

---
+++ react-apps/roi-calculator/src/App.tsx
@@ -0,0 +1,12 @@
+import ROICalculator from './components/ROICalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12">
+      <ROICalculator />
+    </div>
+  );
+}
+
+export default App;
