75d9d387914578a46f45f67dbd91ff3d85566a36
coe-nexus <ops@coveofedu.org>
2025-11-28 16:49:00 +0200

    Adding Communication Game 2, without Thmbnail
    Adding Communication Game 2, without Thmbnail

---
+++ static/embeds/Communication-game-Two/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://ZsB5LhgOPcoAkyyZVDbRJ4I22l-rq4Lx_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Communication Game"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
